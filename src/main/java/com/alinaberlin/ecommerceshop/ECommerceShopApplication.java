884d8ea9709064ca6f18d1786979d7d81f0dea41
alinaberlin <alina.ghetler@gmail.com>
2024-02-21 22:24:14 +0100

    Fix https issue.

--- src/main/java/com/alinaberlin/ecommerceshop/ECommerceShopApplication.java
+++ src/main/java/com/alinaberlin/ecommerceshop/ECommerceShopApplication.java
@@ -4,11 +4,13 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "EShop API", version = "1.0", description = "Eshop Rest API"))
+@OpenAPIDefinition(servers = {@Server(url = "/")},
+        info = @Info(title = "EShop API", version = "1.0", description = "Eshop Rest API"))
 @SecurityScheme(
         name = "Authorization",
         type = SecuritySchemeType.HTTP,
